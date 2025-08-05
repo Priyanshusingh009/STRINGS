66356c82881a5b667a4f5fed1a557135374e2978
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-08-04 23:57:02 -0700

    stringbuilder functions

--- stringBuilder.java
+++ stringBuilder.java
@@ -1,11 +1,24 @@
 public class stringBuilder {
     public static void main(String[] args) {
         int n = 5;
-        StringBuilder str = new StringBuilder(); /* StringBuilder is used to create and build strings efficiently (better than using + repeatedly).
-Here, we create an empty StringBuilder called str. */
-        for (int i = 0; i < n; i++) {
+         /* StringBuilder is used to create and build strings efficiently (better than using + repeatedly).
+        Here, we create an empty StringBuilder called str. */
+        StringBuilder str = new StringBuilder("hello");
+             //append(string) to add words into string
             str.append("hello "); // adding hello to str
-        }
+            //2.setCharAt(index,char)
+            str.setCharAt(2, 'p');
+            //3.insert(ind,char)
+            str.insert(1,'s');
+            //4.deleteCharAt(index)
+            str.deleteCharAt(1);
+            //5.reverse()
+            str.reverse();
+            //6.delete(s,e)
+            str.delete(2, 7);
+            
+        
+
         String s = str.toString(); //this converts the StringBuilder into a regular String and stores it in s.
         System.out.println(s);
     }
