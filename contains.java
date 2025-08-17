3ad5881cd432a0bd73c1cc0e5c2a2c45395c7066
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-08-18 00:09:25 +0530

    verifying whether a sbstring exists or not in sub string

---
+++ contains.java
@@ -0,0 +1,8 @@
+public class contains {
+    public static void main(String[] args) {
+        String str1="hello priyanshu how are you ?";
+        String str2="how";
+        System.out.println(str1.contains(str2));// ---------->true
+    }
+    
+}
