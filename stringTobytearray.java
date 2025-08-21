3ce369b973574e29cda1531b77c173db50d572b0
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-08-21 21:17:44 +0530

    Get Byte Array from String

---
+++ stringTobytearray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class stringTobytearray {
+    public static void main(String[] args) {
+    String text="hello universe";
+    byte[] str_arr=text.getBytes();
+    System.out.println("given string \n "+text);
+    System.out.println("Byte data");
+    System.out.println(" "+Arrays.toString(str_arr));
+        
+    }
+    
+    
+    
+}
