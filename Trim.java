a066c05d2ba806453766f4065b54f83bf22ef861
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-08-22 21:36:18 +0530

    Trim Whitespace

---
+++ Trim.java
@@ -0,0 +1,18 @@
+public class Trim {
+    public static void main(String[] args) {
+        String str = "   Hello World   ";
+        System.out.println("Before trim: [" + str + "]");
+        System.out.println("After trim: [" + str.trim() + "]");
+// Clean extra spaces (start/end).
+
+// Compare strings safely.
+
+// Handle user input (like login forms).
+
+// Correct length calculation.
+
+// Clean data from files, DB, APIs.
+
+// Detect empty inputs (spaces only).
+    }
+}
